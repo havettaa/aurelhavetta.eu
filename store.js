5b873b8a18b8ffa01909d6fb7bb472cb57f02d3d
Aurel Havetta <aurel.havetta>
2023-07-19 18:13:19 +0200

    notification callback

--- store.js
+++ store.js
@@ -1,17 +1,30 @@
-let _objState = { myString: "str"};
+let _notificationCallbacks = [];
+
+let _objState = { myString: "InitialValue"};
 let _mylist = [];
 let _mytext = "";
 let state = { mylist: _mylist, mytext: _mytext, objState: _objState, obj: { refState: _objState } };
 
 export const store = state;
 
+
 export function getStateText() {
   console.info(`getState: ${JSON.stringify(state)}`)
   return state.mytext;
 }
+
+
 export function setStateText(newState) {
   state.mytext = newState;
   _objState.myString = newState;
 
   console.warn(`setState: ${JSON.stringify(state)}`)
+
+  _notificationCallbacks.forEach(func => window.requestAnimationFrame(func));
+}
+
+
+export function storeAddNotificationFunc(func) {
+  console.info(`storeAddNotificationFunc`);
+  _notificationCallbacks.push(func);
 }
